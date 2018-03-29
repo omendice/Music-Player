2de9a5c17c133ddfea699bee9cb8ae133faf1bc5
tzeram <36201715+tzeram@users.noreply.github.com>
2018-03-29 11:18:38 -0400

    Play button plays static
    GUI connects to code

--- mediaplayer.py
+++ mediaplayer.py
@@ -1,27 +1,25 @@
 import sys
 from PyQt4 import QtCore, QtGui, uic
-#import eyed3
+import pygame
 
 form_class = uic.loadUiType("mediaplayer.ui")[0]
-#curr=eyed3.load("")
 
 class MainWindow(QtGui.QMainWindow, form_class):
     def __init__(self, parent = None):
         QtGui.QMainWindow.__init__(self,parent)
         self.setupUi(self)
-        self.pushbutton.clicked.connect(self.PlayButton)
-    def PlayButton(self):
-        if (self.Play.text() == "Play"):
-            self.Play.setText("Pause")
-            mixer.init()
-            mixer.music.load('Music.mp3')
-            mixer.music.play()
-            #self.articstcell.setText(curr.tag.artist)
-            #self.Play.clicked.connect(self.Play_clicked)
-        else:
-            mixer.music.pause()
-            self.Play.setText("Play")
+        self.PlayButton.clicked.connect(self.PlayButtons)
         
+    def PlayButtons(self):
+        if (self.PlayButton.text() == "Play"):
+            self.PlayButton.setText("Play")
+            pygame.mixer.init()
+            pygame.mixer.music.load("02 Strawberry Swing.mp3")
+            pygame.mixer.music.play()
+        else:
+            pygame.mixer.music.pause()
+            self.PlayButton.setText("Play")
+
 app = QtGui.QApplication(sys.argv)
 myWindow = MainWindow()
 myWindow.show()
